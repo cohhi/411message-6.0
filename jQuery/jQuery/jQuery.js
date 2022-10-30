b298bad461552d7543d7c2861e72f40a3b46a680
steam-404 <71576964+steam-404@users.noreply.github.com>
2022-10-30 15:00:15 +0800

    update

--- jQuery/jQuery/jQuery.js
+++ jQuery/jQuery/jQuery.js
@@ -1,7 +1,7 @@
 //author:steam-404
 
 //调用,测试/开发版jQuery
-import '../development jQuery v3.6.1'
+import '../development-jQueryV3.6.1/jQuery.js'
 
 //调用,生产力版jQuery
-//import '../production jQuery 3.6.1'
+//import '../production-jQueryV3.6.1'
